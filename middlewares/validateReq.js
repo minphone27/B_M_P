25f22548c75857be910dea88cbebc93a032ef508
SuSandi Linn <www.susandilinn2018.com>
2022-01-11 21:29:10 +0630

    chnage password

--- middlewares/validateReq.js
+++ middlewares/validateReq.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 const validateReq = (req, res, next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-
-        // fs.unlinkSync(req.file.path);
-
+        if(req.file){
+            fs.unlinkSync(req.file.path);
+        }
         return res.status(400).json({errors:errors.array()[0].msg})
     }
     next();
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,17 @@
-const { getAllUsers, getSingleUser, getMyProfile, UserSignUp, UserSignIn, UserSignOut, UpdateUser, DeleteUser, assignWork, unassignWork, assignRole, unassignRole, togglePublic } = require("../controllers/userController");
+const { getAllUsers, 
+    getSingleUser, 
+    getMyProfile, 
+    UserSignUp, 
+    UserSignIn, 
+    UserSignOut, 
+    UpdateUser,
+    changePw, 
+    DeleteUser, 
+    assignWork, 
+    unassignWork, 
+    assignRole, 
+    unassignRole,
+    toggleAdmin } = require("../controllers/userController");
 const fileUpload = require("../middlewares/fileUpload");
 const validateReq = require("../middlewares/validateReq");
 const signUpSchema = require("../schemas/userSchema/signUpSchema");
@@ -8,21 +21,23 @@ const signInSchema = require("../schemas/userSchema/signInSchema");
 const auth = require("../middlewares/authentication");
 const adminMiddleWare = require("../middlewares/admin");
 const updateSchema = require("../schemas/userSchema/updateSchema");
+const changePwSchema = require("../schemas/userSchema/changePwSchema");
 
 const router = express.Router();
 
-router.get("/", auth, adminMiddleWare,getAllUsers)
+router.get("/", auth, adminMiddleWare,getAllUsers);
 router.get("/mine", auth, getMyProfile);
 router.get("/:id", auth, adminMiddleWare,getSingleUser);
 router.post("/signUp", fileUpload.single("avatar"), signUpSchema, validateReq, UserSignUp);
 router.post("/signIn", signInSchema, validateReq, UserSignIn);
 router.post("/signOut", auth, UserSignOut);
 router.put("/", auth, fileUpload.single("avatar"), updateSchema, validateReq, UpdateUser);
+router.put("/changePw", changePwSchema, validateReq, auth, changePw );
 router.delete("/", auth, DeleteUser);
 router.post("/:id",assignRole);
 router.delete("/unassign_role/:id", unassignRole);
 router.post("/:id",assignWork);
 router.delete("/unassign_work/:id",unassignWork);
-router.put("/admin/:id", auth, togglePublic);
+router.put("/admin/:id", auth, toggleAdmin);
 
 module.exports = router;
